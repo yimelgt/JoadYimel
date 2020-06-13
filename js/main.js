25d4f6ca2861945d23a8d02905023ce5ef1f12aa
yimelgt <66337117+yimelgt@users.noreply.github.com>
2020-06-13 04:18:59 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    fitToSection: false,
+    fitToSectionDelay: 300,
+    easing: 'easeInOutCubic',
+    navigation: true,
+    anchors: ['Home', 'Sobre-mi', 'Galeria', 'Contacto'],
+    navigationTooltips: ['Home', 'Sobre mi', 'Galería', 'Contacto'],
+    showActiveTooltip: false,
+    menu: '#menu',
+    scrollSpeed: 1000,
+    controlArrows: false,
+    slidesNavigation: true,
+    css3: true,
+    easingcss3: 'ease-out',
+    loopBottom: true,
+    keyboardScrolling: true,
+	animateAnchor: true,
+    recordHistory: false,
+    responsiveWidth: 0,
+    responsiveHeight: 0,
+});
+
